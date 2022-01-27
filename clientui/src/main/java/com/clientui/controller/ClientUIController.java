e76fafc7025e8de68783ac4f9c736a9a9b859a4b
Leslie Gayard <leslie.123@hotmail.fr>
2022-01-27 14:32:54 +0100

    Feign htttp client single product page

--- clientui/src/main/java/com/clientui/controller/ClientUIController.java
+++ clientui/src/main/java/com/clientui/controller/ClientUIController.java
@@ -5,6 +5,7 @@ import com.clientui.proxies.MicroserviceProduitProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,11 +16,24 @@ public class ClientUIController {
     @Autowired
     private MicroserviceProduitProxy produitProxy;
 
+    /* HOME PAGE */
     @RequestMapping("/")
     public String acceuil(Model model){
 
         List<ProductBean>produits = produitProxy.listeDesProduits();
         model.addAttribute("produits",produits);
         return "Accueil";
+    }
+
+
+    /* PRODUCT PAGE */
+    @RequestMapping("/details-produit/{id}")
+    public String ficheProduit(@PathVariable int id, Model model){
+
+        ProductBean produit = produitProxy.recupererUnProduit(id);
+
+        model.addAttribute("produit", produit);
+
+        return "FicheProduit";
     }
 }
